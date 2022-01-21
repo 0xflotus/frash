193bcec7ca39a1ee6b83b51fc22d1f33c8be8c6b
0xflotus <0xflotus@gmail.com>
2022-01-21 15:44:59 +0100

    adjustments

--- frash/util.py
+++ frash/util.py
@@ -1,16 +1,22 @@
 import sys
 
 def normalize_to_hex(input):
-    try:
-        output = float.hex(float(input))
-        out_arr = output.split('p')
-        out_arr[0] = out_arr[0].rstrip('0')
-        out_arr[1] = out_arr[1].lstrip('+')
-        output = out_arr[0] + 'p' + out_arr[1]
-        return output
-    except ValueError:
-        print("Wrong input!")
-        sys.exit(-1)
+    if input in ['.0', '0.']:
+        return '0x00'
+    else:
+        try:
+            return hex(int(input))
+        except ValueError:
+            try:
+                output = float.hex(float(input))
+                out_arr = output.split('p')
+                out_arr[0] = out_arr[0].rstrip('0')
+                out_arr[1] = out_arr[1].lstrip('+')
+                output = out_arr[0] + 'p' + out_arr[1]
+                return output
+            except ValueError:
+                print("Wrong input!")
+                sys.exit(-1)
 
 def from_hex(input):
     try:
